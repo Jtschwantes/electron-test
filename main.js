a91332f8144c17cc0bc06a7109312474886a94e2
Seth Bolander <bol15015@byui.edu>
2021-03-27 22:54:56 -0600

    a couple changes to get it to work

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 const { app, BrowserWindow } = require("electron");
 const bot = require('robotjs')
-const ioh = require('iohook')
+// const ioh = require('iohook')
 const path = require("path");
 
+let mainWindow;
+
 const loadMainWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     // fullscreen: true,
     width: 1200,
     height: 800,
-    frame: false,
-    transparent: true,
+    // frame: false,
+    // transparent: true,
     webPreferences: {
       nodeIntegration: true
     }
   });
-  mainWindow.webContents.openDevTools()
-  mainWindow.setIgnoreMouseEvents(false, {forward: true})
-  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.webContents.openDevTools();
+  mainWindow.setIgnoreMouseEvents(false, {forward: true});
+  mainWindow.loadFile("index.html");
+
+  // clear window on clean exit
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
 }
 
-app.on("ready", loadMainWindow);
+app.whenReady().then(loadMainWindow);
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
-    app.exit();
+    app.quit();
   }
 });
 app.on("activate", () => {
